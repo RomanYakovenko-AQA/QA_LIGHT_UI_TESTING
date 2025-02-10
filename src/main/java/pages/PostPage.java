fc3e7d7f85a12523edf57ac36703e79604b9309a
Roman Yakovenko <yakovenkors@gmail.com>
2025-02-10 12:54:11 +0200

    19012025

--- src/main/java/pages/PostPage.java
+++ src/main/java/pages/PostPage.java
@@ -1,15 +1,51 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import pages.elements.HeaderElement;
 
 public class PostPage extends ParentPage {
+
+    @FindBy(xpath = "//div[@class='alert alert-success text-center']")
+    private WebElement successMessage;
+
+    @FindBy(xpath = "//button[@class='delete-post-button text-danger']")
+    private WebElement buttonDeletePost;
+
     public PostPage(WebDriver webDriver) {
         super(webDriver);
+    }
+
+    public HeaderElement getHeaderElement() {
+        return new HeaderElement(webDriver);
     }
 
     public PostPage checkIsRedirectToPostPage() {
         //TODO checkUrl
         //TODO check some element
         return this;
+    }
+    /*
+    * Method for checking is success message displayed
+    * doesn't check text in the message
+     */
+
+    public PostPage checkIsSuccessMessageDisplayed() {
+        Assert.assertTrue("Success message is not displayed", isElementVisible(successMessage));
+        return this;
+    }
+
+    public PostPage checkTextInSuccessMessage(String expectedMessageText) {
+        String actualText = successMessage.getText();
+        Assert.assertEquals("Text in message", expectedMessageText, actualText);
+        return this;
+    }
+
+    public MyProfilePage clickOnDeleteButton() {
+
+        clickOnElement(buttonDeletePost);
+        return new MyProfilePage(webDriver);
     }
 }
